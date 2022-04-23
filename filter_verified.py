0bf6a334f7076db0ea31e75512ec2e63743b6478
Andre Hu <andrebhu@gmail.com>
2022-04-23 12:16:15 -0400

    Fixes

--- filter_verified.py
+++ filter_verified.py
@@ -7,37 +7,143 @@ import requests
 
 from time import sleep
 from itertools import cycle
-from dotenv import load_dotenv
+from datetime import datetime
 
-load_dotenv()
+# from dotenv import load_dotenv
+from dotenv import dotenv_values
 
-GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
-GITHUB_TOKEN2 = os.getenv('GITHUB_TOKEN2')
-GITHUB_TOKEN3 = os.getenv('GITHUB_TOKEN3')
-GITHUB_TOKEN4 = os.getenv('GITHUB_TOKEN4')
+# only store Github tokens in .env
+secrets = dotenv_values()
+tokens = list(secrets.values())
 
-tokens = [GITHUB_TOKEN, GITHUB_TOKEN2, GITHUB_TOKEN3, GITHUB_TOKEN4]
 token_cycle = cycle(tokens)
 SLEEP_RATE = 0.8 / len(tokens) # 0.72 3600 seconds / 5000 requests, 0.8 for safe measure
 
+FEATURES = [
+    "login",
+    "id",
+    "name",
+    "company",
+    "blog",
+    "email",
+    "twitter_username",
+    "is_verified",
+    "has_organization_projects",
+    "has_repository_projects",
+    "public_repos",
+    "public_gists",
+    "html_url",
+    "created_at",
+    "updated_at",
+    "type",
+]
 
-def checkOrg(orgname):
-    pass    
 
+def checkOrg(orgname):    
+    # If not found on normal GitHub, save an API call and return
+    r = requests.get(f"https://github.com/{orgname}")
+    if int(r.status_code) == 404:
+        return
 
+    headers = {
+        'Authorization': f'token {next(token_cycle)}'
+    }
+    data = requests.get(f"https://api.github.com/orgs/{orgname}", headers=headers).json()
+
+    if data['is_verified']:
+        print(f"[{datetime.now()}] {data['login']} {data['id']}")
+        return data
 
 
 def main():
+    
     with open("organizations-4-22-2022.csv", "r") as infile:
         reader = csv.reader(infile)
         # skip headers 
         next(reader)
 
-        print(next(reader)[0])
+        # If file already exists
+        if os.path.exists('verified_organizations.csv'):
+            # Find last org id
+            last_id = 0
+            with open('verified_organizations.csv', 'rb') as f:
+                try:
+                    f.seek(-2, os.SEEK_END)
+                    while f.read(1) != b'\n':
+                        f.seek(-2, os.SEEK_CUR)
+                except OSError:
+                    f.seek(0)
 
-        with open("verified_organizations.csv", "w") as outfile:
-            pass
+                last_line = f.readline().decode().split(',')
+            
+                last_id = int(last_line[1])
 
+            # Skip rows until org id
+            row = next(reader)
+            while int(row[1]) < last_id:
+                row = next(reader)
+
+            print(f"[{datetime.now()}] Last entry {row}")
+            
+            # Appending to already existing file
+            with open("verified_organizations.csv", "a") as outfile:
+                writer = csv.DictWriter(outfile, fieldnames=FEATURES)
+
+                while True:
+                    name = next(reader)[0]
+                    data = checkOrg(name)
+
+                    if data:
+                        writer.writerow({
+                            "login": data['login'],
+                            "id": data['id'],
+                            "name": data['name'],
+                            "company": data['company'],
+                            "blog": data['blog'],
+                            "email": data['email'],
+                            "twitter_username": data['twitter_username'],
+                            "is_verified": data['is_verified'],
+                            "has_organization_projects": data['has_organization_projects'],
+                            "has_repository_projects": data['has_repository_projects'],
+                            "public_repos": data['public_repos'],
+                            "public_gists": data['public_gists'],
+                            "html_url": data['html_url'],
+                            "created_at": data['created_at'],
+                            "updated_at": data['updated_at'],
+                            "type": data['type'],
+                        })
+
+                    sleep(SLEEP_RATE)
+
+        else:
+            with open("verified_organizations.csv", "w") as outfile:
+                writer = csv.DictWriter(outfile, fieldnames=FEATURES)
+                writer.writeheader()
+
+                while True:
+                    name = next(reader)[0]
+                    data = checkOrg(name)
+
+                    if data:
+                        writer.writerow({
+                            "login": data['login'],
+                            "id": data['id'],
+                            "name": data['name'],
+                            "company": data['company'],
+                            "blog": data['blog'],
+                            "email": data['email'],
+                            "twitter_username": data['twitter_username'],
+                            "is_verified": data['is_verified'],
+                            "has_organization_projects": data['has_organization_projects'],
+                            "has_repository_projects": data['has_repository_projects'],
+                            "public_repos": data['public_repos'],
+                            "public_gists": data['public_gists'],
+                            "html_url": data['html_url'],
+                            "created_at": data['created_at'],
+                            "updated_at": data['updated_at'],
+                            "type": data['type'],
+                        })
+                    sleep(SLEEP_RATE)
 
 
 
